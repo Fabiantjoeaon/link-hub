e26c681772ac849c0aea420df32cd020bff45d9c
Fabian Tjoe-A-On <fabian@chainels.com>
2017-03-06 20:42:46 +0100

    Initial commit

---
+++ webpack.dev.config.js
@@ -0,0 +1,42 @@
+'use strict';
+const path = require('path');
+const webpack = require('webpack');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+const SRC_DIR = path.resolve(__dirname, './src');
+const DIST_DIR = path.resolve(__dirname, './dist');
+const STATIC_DIR = path.resolve(__dirname, './static');
+
+module.exports = {
+    entry: [
+        'webpack-hot-middleware/client',
+        SRC_DIR + '/index'
+    ],
+    output: {
+        path: path.join(__dirname, '/dist'),
+        publicPath: '/static/',
+        // Gets name of object prop, bundle in this case
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'stage-0', 'react']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new (webpack.optimize.OccurenceOrderPlugin || webpack.optimize.OccurrenceOrderPlugin)()
+    ],
+    // resolve: {
+    //     modulesDirectories: ['src', 'node_modules']
+    // }
+    // plugins: [
+    //     new WriteFilePlugin()
+    // ]
+};
