8fd5930e4e7e7f37738b6c84595269c24692ed75
Fabian Tjoe-A-On <fabian@chainels.com>
2017-03-30 10:42:31 +0200

    removed group and link actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,4 @@
 import * as actionTypes from './constants';
-import v4 from 'node-uuid';
-
-export const addLink = (url, description, group) => ({
-    type: actionTypes.ADD_LINK,
-    id: v4(),
-    url,
-    description,
-    group
-});
-
-export const addGroup = (name, color, dateAdded) => ({
-    type: actionTypes.ADD_GROUP,
-    id: v4(),
-    name,
-    color,
-    dateAdded
-});
 
 export const changeBackgroundColor = (backgroundColor) => ({
     type: actionTypes.CHANGE_DASHBOARD_BACKGROUND_COLOR,
